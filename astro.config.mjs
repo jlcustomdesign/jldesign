8a1bf44619755aa05f989cf80ee7b0ec6c11631d
RobertGyorgy <63159991+RobertGyorgy@users.noreply.github.com>
2026-02-07 20:09:29 +0200

    fix: add canonical site URL for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jl-design.vercel.app',
   output: 'server',
   adapter: vercel(),
   integrations: [
