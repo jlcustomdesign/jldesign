93d531ed90bd2fbb871f3783e9141ec08338b7f5
RobertGyorgy <63159991+RobertGyorgy@users.noreply.github.com>
2026-05-08 00:09:11 +0300

    fix: update site URL to mobilapersonalizatabrasov.ro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jlmobila.ro', // Updated to custom domain
+  site: 'https://mobilapersonalizatabrasov.ro/', // Updated to custom domain
   output: 'server',
   adapter: vercel(),
   integrations: [
